4907fb0b757332c8aa988579988c5309e5862246
Ubriacopo <65170353+Ubriacopo@users.noreply.github.com>
2020-12-05 15:32:03 +0100

    WIP
    AAAAA

--- Database/Creation.sql
+++ Database/Creation.sql
@@ -34,7 +34,7 @@
     drop table if exists SuperOrdine;
 
     drop table if exists Tag;
-
+    drop table if exists Label;
 
 
     create table EnteRiconosciuto(
@@ -65,6 +65,7 @@
     ) engine = InnoDB;
 
     create table CertificatoUtente(
+
         UserID INT UNSIGNED NOT NULL PRIMARY KEY,
         CertID INT UNSIGNED NOT NULL PRIMARY KEY,
 
@@ -86,4 +87,102 @@
 
         FOREIGN KEY (SeguaceID) REFERENCES Utente(ID) ON DELETE CASCADE,
         FOREIGN KEY (SeguitoID) REFERENCES Utente(ID) ON DELETE CASCADE
+    ) engine = InnoDB;
+
+    create table Label( /* Etttichetta (relativa a uno o molti tag)*/
+        ID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        text VARCHAR(255) NOT NULL
+    ) engine = InnoDB;
+
+    create table Tag(
+        ID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        nome VARCHAR(20) UNIQUE NOT NULL,
+
+        LabelID INT UNSIGNED,
+        FOREIGN KEY (LabelID) REFERENCES Label(ID) ON DELETE SET NULL
+    ) engine = InnoDB;
+
+    /* Elemetni degli uccelli.*/
+
+    create table Ordine(
+        TagID INT UNSIGNED NOT NULL PRIMARY KEY,
+        nomeScientifico VARCHAR(40) NOT NULL UNIQUE,
+
+        FOREIGN KEY (TagID) REFERENCES Tag(ID) ON DELETE CASCADE
+    ) engine = InnoDB;
+
+
+    create table Famiglia(
+        TagID INT UNSIGNED NOT NULL PRIMARY KEY,
+        OrdID INT UNSIGNED NOT NULL PRIMARY KEY,
+
+        nomeScientifico VARCHAR(40) NOT NULL,
+        caratteristicheComuni TEXT,
+
+        /** Se elimino dal sistema un ordine elimino anche tutte le famiglie
+            ad esso relativo. Mi pare giusto così.*/
+        FOREIGN KEY (TagID) REFERENCES Tag(ID) ON DELETE CASCADE ,
+        FOREIGN KEY (OrdID) REFERENCES Ordine(TagID) ON DELETE CASCADE
+
+    ) engine = InnoDB;
+
+
+    create table Genere(
+        tagID INT UNSIGNED NOT NULL PRIMARY KEY,
+        famID INT UNSIGNED NOT NULL,
+
+        nomeScientifico VARCHAR(40) NOT NULL,
+
+        FOREIGN KEY (tagID) REFERENCES Tag(ID) ON DELETE CASCADE ,
+        FOREIGN KEY (famID) REFERENCES Ordine(TagID) ON DELETE CASCADE
+
+    ) engine = InnoDB;
+
+    create table Conservazione(
+        codice varchar(2) NOT NULL PRIMARY KEY,
+
+        nome varchar(20) NOT NULL UNIQUE,
+        probEstinzione integer
+
+    ) engine = InnoDB;
+
+    create table Specie(
+        tagID int UNSIGNED NOT NULL PRIMARY KEY,
+        genID int UNSIGNED NOT NULL,
+
+        nomeScientifico varchar(40) NOT NULL,
+        percorsoImmagine varchar(80) NOT NULL,
+
+        conservazioneID varchar(2) NOT NULL,
+
+        pesoMedio int UNSIGNED NOT NULL,
+        altezzaMedia int UNSIGNED NOT NULL,
+        descrizione text NOT NULL,
+
+        FOREIGN KEY (tagID) REFERENCES Tag(ID),
+        FOREIGN KEY (genID) REFERENCES Genere(tagID) ON DELETE CASCADE,
+
+        FOREIGN KEY (conservazioneID) references Conservazione(Codice) ON DELETE RESTRICT
+    ) engine = InnoDB;
+
+
+    create table ZonaGeografica(
+
+        tagID int UNSIGNED NOT NULL PRIMARY KEY,
+
+        nome varchar(40) NOT NULL,
+        continente enum('Africa','America del nord', 'Sud America',
+            'Asia', 'Europa', 'Oceania', 'Antartide'),
+
+        FOREIGN KEY (tagID) REFERENCES Tag(ID) ON DELETE CASCADE
+    ) engine = InnoDB;
+
+    create table Residenza(
+
+        specieID int UNSIGNED NOT NULL PRIMARY KEY,
+        zonaID int UNSIGNED NOT NULL PRIMARY KEY,
+
+        periodoInizio date NOT NULL,
+        periodoFine date NOT NULL CHECK ( periodoFine > periodoInizio ) /* Data fine deve venire dopo di inizio*/
+
     ) engine = InnoDB;

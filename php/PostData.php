8955685e6b16c98a7f30c47de29ce2c628bf6caa
Ubriacopo <65170353+Ubriacopo@users.noreply.github.com>
2020-12-15 11:43:31 +0100

    Elemento di post.

---
+++ php/PostData.php
@@ -0,0 +1,22 @@
+<?php
+    require_once "DataElement.php";
+    class PostData extends DataElement{
+
+        protected function loadData() {
+            // TODO: Implement loadData() method.
+        }
+
+        public function checkID($id) {
+            // TODO: Implement checkID() method.
+        }
+
+        public function baseData() {
+            // TODO: Implement baseData() method.
+        }
+
+        public function fullData() {
+            // TODO: Implement fullData() method.
+        }
+    }
+
+?>

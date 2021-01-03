934041f3363415d406d9f635dd967e4e8927d150
MicheGit <chrimiche@hotmail.it>
2021-01-03 17:23:43 +0100

    Post cards php

--- php/control/components/feed/PostCard.php
+++ php/control/components/feed/PostCard.php
@@ -1,10 +1,45 @@
 <?php
+require_once __ROOT__.'\control\components\summaries\PageFiller.php';
 
 
-class PostCard extends Component {
+class PostCard extends PageFiller {
 
-    public function build()
-    {
-        // TODO: Implement build() method.
+    private $HTML;
+
+    private $postElement;
+
+    public function __construct($postId) {
+        // construct parent
+        parent::__construct(file_get_contents(__ROOT__."/view/modules/PostCard.xhtml"));
+        // get parent's layout
+        $this->HTML = $this->baseLayout();
+        // get post's attributes
+        $this->postElement = new PostElement();
+        $this->postElement->loadElement($postId);
+    }
+
+    public function build() {
+        foreach ($this->resolveData() as $placeholder => $value) {
+            $this->HTML = str_replace($placeholder, $value, $this->HTML);
+        }
+        return $this->HTML;
+    }
+
+    public function resolveData() {
+        $ritorno = [];
+
+        foreach ($this->postElement->getData() as $key => $value) {
+            if ($key === "immagini") {
+                $ritorno['{linkImage}'] = $value[0];
+            } else {
+                if ($key === 'UserID') {
+                    $author = new UserElement($value);
+
+                    $ritorno['{authorName}'] = $author->getData()['nome'];
+                }
+                $ritorno['{' . $key . '}'] = $value;
+            }
+        }
+        return $ritorno;
     }
 }

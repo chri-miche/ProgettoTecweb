bdb18c4bf2c0699c18485ca305b34265637cb33f
Ubriacopo <65170353+Ubriacopo@users.noreply.github.com>
2021-01-12 15:41:25 +0100

    Modifiche varie

--- php/control/components/feed/PostCard.php
+++ php/control/components/feed/PostCard.php
@@ -3,29 +3,30 @@ require_once __ROOT__.'\control\components\summaries\PageFiller.php';
 
 
 class PostCard extends PageFiller {
-
-    private $HTML;
-
     private $postElement;
+
 
     public function __construct($postId) {
         // construct parent
         parent::__construct(file_get_contents(__ROOT__.'\view\modules\feed\PostCard.xhtml'));
-        // get parent's layout
-        $this->HTML = $this->baseLayout();
+
         // get post's attributes
         $this->postElement = new PostElement();
         $this->postElement->loadElement($postId);
     }
 
     public function build() {
-        foreach ($this->resolveData() as $placeholder => $value) {
-            $this->HTML = str_replace($placeholder, $value, $this->HTML);
-        }
-        return $this->HTML;
+        // Get parent layout.
+        $HTML = $this->baseLayout();
+
+        foreach ($this->resolveData() as $placeholder => $value)
+            $HTML = str_replace($placeholder, $value, $HTML);
+
+        return $HTML;
     }
 
     public function resolveData() {
+
         $ritorno = [];
 
         foreach ($this->postElement->getData() as $key => $value) {

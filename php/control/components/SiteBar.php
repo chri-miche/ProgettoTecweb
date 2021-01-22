adeb10b857e258e6d5fcae0af8ada02f2dafa2bc
Ubriacopo <65170353+Ubriacopo@users.noreply.github.com>
2021-01-23 00:26:42 +0100

    LA PURGA COMINCIA DOMANI

--- php/control/components/SiteBar.php
+++ php/control/components/SiteBar.php
@@ -4,10 +4,9 @@
     require_once __ROOT__.'\control\SessionUser.php';
     require_once __ROOT__.'\control\components\browsers\NavigationButton.php';
 
-    class SiteBar extends Component {
+    require_once __ROOT__ . '\model\DAO\UserDAO.php';
 
-        private $HTML; /** Spostare questo a private in Component e renderla una classe?
-                        Cosi da evitare che possa essere modificato una volta inizilizzata.*/
+    class SiteBar extends Component {
 
         private $user;
         /**
@@ -17,13 +16,12 @@
 
         /** TODO: Give user as parameter by reference? Avoid multiple definitons of SessionUser.
          * @param string|null $HTMLcontent
-         * @param string $position
-         */
+         * @param string $position */
         public function __construct(string $position, string $HTMLcontent = null) {
 
-            parent::__construct(isset($HTMLcontent) ? $HTMLcontent : file_get_contents(__ROOT__.'\view\modules\SiteBar.xhtml'));
-            $this->user = new SessionUser();
+            parent::__construct($HTMLcontent ?? file_get_contents(__ROOT__.'\view\modules\SiteBar.xhtml'));
 
+            $this->user = new SessionUser();
             $this->position = $position;
 
         }
@@ -34,20 +32,22 @@
             $baseLayout = $this->baseLayout();
 
             /** To make code tidied up count the black space of the opened tag before.*/
-            if(!$this->user->getUser()->getId()){
+            if($this->user->userIdentified()){
 
                 $contentHTML = file_get_contents(__ROOT__.'\view\modules\LoggedOutActions.xhtml');
 
             } else {
+                $userVO = $this->user->getUser();
                 $contentHTML = file_get_contents(__ROOT__.'\view\modules\LoggedInActions.xhtml');
-                $username = $this->user->getUser()->nome;
-                $userid = $this->user->getUser()->ID;
-                $contentHTML = str_replace("{username}", $username, $contentHTML);
-                $contentHTML = str_replace("{userid}", $userid, $contentHTML);
+
+                $contentHTML = str_replace("{username}", $userVO->getNome(), $contentHTML);
+                $contentHTML = str_replace("{userid}", $userVO->getId(), $contentHTML);
 
                 if($this->user->getAdmin()) {
+
                     $adminButton = new NavigationButton('Admin', 'Admin.php');
                     $contentHTML = str_replace('<admin />', $adminButton->build(), $contentHTML);
+
                 }
 
             }

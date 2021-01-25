a959a488da9176caaf018223ba3c0b3e26f87638
Ubriacopo <65170353+Ubriacopo@users.noreply.github.com>
2021-01-25 11:55:04 +0100

    fsf

--- php/view/pages/UserPage.php
+++ php/view/pages/UserPage.php
@@ -7,31 +7,41 @@
     require_once __ROOT__ . '\control\components\SiteBar.php';
     require_once __ROOT__ . '\control\components\BreadCrumb.php';
 
+    require_once __ROOT__.'\control\SessionUser.php';
     /** Page specific. */
     require_once __ROOT__ . '\control\components\summaries\UserPage.php';
 
     $basePage = file_get_contents(__ROOT__.'\view\BaseLayout.xhtml');
 
-    if (isset($_POST['submit-profile-pic']) && isset($_GET['id'])) {
-        $name = basename($_FILES["input-file"]["name"]);
-        $tmp_name = basename($_FILES["input-file"]["tmp_name"]);
+    $sessionUser = new SessionUser();
 
-        $rootParent = dirname(__ROOT__);
-        $proposedPath = DIRECTORY_SEPARATOR . "res" . DIRECTORY_SEPARATOR . "$name";
+    if($sessionUser->userIdentified()) {
 
-        $tentativi = 0;
-        while (file_exists($rootParent.$proposedPath)) {
-            $tentativi++;
-            $proposedPath = "res" . DIRECTORY_SEPARATOR . ($tentativi === 0 ? '' : $tentativi) . "$name";
-        }
+        $userVO = $sessionUser->getUser();
+        if (isset($_POST['submit-profile-pic']) && isset($_GET['id']) && $_GET['id'] == $userVO->getId()){
 
-        if(move_uploaded_file($tmp_name, $rootParent.$proposedPath)) {
-            // TODO nome file
-        } else {
-            throw new Exception("Non è stato possibile salvare le foto");
+            $name = basename($_FILES["input-file"]["name"]);
+            $tmp_name = basename($_FILES["input-file"]["tmp_name"]);
+
+            $rootParent = dirname(__ROOT__);
+            $proposedPath = DIRECTORY_SEPARATOR . "res" . DIRECTORY_SEPARATOR . "$name";
+
+            $tentativi = 0;
+            while (file_exists($rootParent . $proposedPath)) {
+                $tentativi++;
+                $proposedPath = "res" . DIRECTORY_SEPARATOR . ($tentativi === 0 ? '' : $tentativi) . "$name";
+            }
+
+            if (move_uploaded_file($tmp_name, $rootParent . $proposedPath)) {
+
+                $userVO->setImmagine(addslashes($proposedPath));
+                $result = (new UserDAO())->save($userVO);
+
+            } else {
+                throw new Exception("Non è stato possibile salvare le foto");
+            }
         }
     }
-
     $page = new BasePage($basePage);
 
     $page->addComponent(new SiteBar('User'));

041e245348f7a78d53b5c5538ea6c8f68c0bd708
Ubriacopo <65170353+Ubriacopo@users.noreply.github.com>
2021-05-26 19:11:23 +0200

    Error pages and component

--- BirdWatchers/Application/error/BirdError.php
+++ BirdWatchers/Application/error/BirdError.php
@@ -2,7 +2,16 @@
 
 require_once __DIR__ . "/../Component.php";
 class BirdError extends Component {
-    public function __construct(string $HTML, string $message, string $title, string $redirect, int $code) {
-        parent::__construct($HTML ?? __DIR__ . "/error.xhtml");
+
+    private $data;
+
+    public function __construct(?string $HTML, string $message, string $title, string $redirect, int $code) {
+
+        parent::__construct($HTML ?? file_get_contents(__DIR__ . "/error.xhtml"));
+        $this->data = array('{title}' => $title, '{description}' => $message, '{code}' => $code, '{redirect}'=> $redirect);
+    }
+
+    public function resolveData(){
+        return $this->data;
     }
 }

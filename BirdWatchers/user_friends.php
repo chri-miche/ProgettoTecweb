464c8dfa4b9ec7ffb4b6f88ada828ca31c6c3467
Ubriacopo <65170353+Ubriacopo@users.noreply.github.com>
2021-06-01 13:47:27 +0200

    Fix vari

--- BirdWatchers/user_friends.php
+++ BirdWatchers/user_friends.php
@@ -14,7 +14,7 @@ try {
     $page = new BasePage($basePage);
 
     $page->addComponent(new SiteBar('user_friends'));
-    $page->addComponent(new BreadCrumb(array('Utente' => "user_page.php?id=$id", 'Amici' => '')));
+    $page->addComponent(new BreadCrumb(array('Utente' => "user_page.php?id=$id", "Amici di $pageUser->nome" => '')));
 
     try {
 
